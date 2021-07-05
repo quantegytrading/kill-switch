2ddadd0a3b08be0f1108753dcc634f5ee924c3f9
Adam Smith <adam_smith3@comcast.com>
2021-07-05 10:16:02 -0400

    rework

---
+++ kill-switch.py
@@ -0,0 +1,46 @@
+# kill-switch.py
+
+import os
+import ccxt
+from ccxt import InvalidOrder, InsufficientFunds
+
+
+def init_exchange():
+    exchange_class = getattr(ccxt, 'binanceus')
+    exchange = exchange_class({
+        'apiKey': os.environ['key'],
+        'secret': os.environ['secret'],
+        'timeout': 30000,
+        'enableRateLimit': True,
+    })
+    exchange.loadMarkets()
+    return exchange
+
+
+def truncate_float(f) -> float:
+    ff = format(f, '.10f')
+    s = str(ff)
+    xs = s.split('.')
+    return float(xs[0] + '.' + xs[1][:6])
+
+
+def main():
+    base_currency = 'USD'
+
+    exchange = init_exchange()
+    symbols = exchange.fetchBalance()
+    for symbol in symbols.get('free'):
+        if symbol not in [base_currency]:
+            free = truncate_float(symbols.get(symbol).get('free'))
+            if free > 0:
+                print(symbol + ": " + str(free))
+                try:
+                    order = exchange.createMarketSellOrder(symbol + '/' + base_currency, free)
+                    print("Sell:")
+                    print(order)
+                except InvalidOrder as e:
+                    print(e)
+                except InsufficientFunds as e:
+                    print(e)
+
+main()
